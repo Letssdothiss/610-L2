72039baad87a83000eacabf9aaff5ca4e7387991
Emil Jonasson Östvall <ej224jt@student.lnu.se>
2024-09-30 21:16:59 +0200

    Start salt module, method definitions, implementation next.

---
+++ src/Test/java/stringCryption/Salt/SaltTest.java
@@ -0,0 +1,5 @@
+package Test.java.stringCryption.Salt;
+
+public class SaltTest {
+    
+}
--- src/main/java/stringCryption/Salt/Salt.java
+++ src/main/java/stringCryption/Salt/Salt.java
@@ -1,5 +1,39 @@
 package main.java.stringCryption.Salt;
 
 public class Salt {
-    
+  private String salt;
+
+  /**
+   * Generate a random salt at specified length.
+   */
+  private void generateSalt(int lengthOfSalt) {
+  }
+
+  /**
+   * Combine input with salt.
+   */
+  private String combineSaltAndInput(String salt, String input) {
+    return input;
+  }
+
+  /**
+   * Remove salt from input.
+   */
+  private String removeSaltFromInput(String salt, String input) {
+    return input;
+  }
+
+  /**
+   * Add Salt.
+   */
+  public String addSalt(String input, int lengthOfSalt) {
+    return input;
+  }
+
+  /**
+   * Remove Salt.
+   */
+  public String removeSalt(String input) {
+    return input;
+  }
 }
