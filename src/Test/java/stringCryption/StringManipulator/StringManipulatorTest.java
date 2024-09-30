1657fb22fc76aceea6c4c95cd486727aad2630c3
Emil Jonasson Östvall <ej224jt@student.lnu.se>
2024-09-30 14:03:34 +0200

    Implementing unit tests.

--- src/Test/java/stringCryption/StringManipulator/StringManipulatorTest.java
+++ src/Test/java/stringCryption/StringManipulator/StringManipulatorTest.java
@@ -14,16 +14,39 @@ public class StringManipulatorTest {
     manipulator = new StringManipulator();
   }
     
-  // Test both ways to exhaust test, since the method is used for both encryption and decryption.
   @Test
-  public void testReverseString() {}
+  public void testReverseString() {
+    // Testing both ways to exhaust test, since the method is used for both encryption and decryption.
+    assertEquals(".ecnetnes a esreveR", manipulator.reverseString("Reverse a sentence."));
+    assertEquals("Reverse a sentence.", manipulator.reverseString(".ecnetnes a esreveR"));
+    assertEquals("abcde", manipulator.reverseString("edcba"));
+    assertEquals("edcba", manipulator.reverseString("abcde"));
+    assertEquals("a", manipulator.reverseString("a"));
+    assertEquals("A", manipulator.reverseString("A"));
+    assertEquals("", manipulator.reverseString(""));
+  }
 
   @Test
-  public void testDuplicateString() {}
+  public void testDuplicateString() {
+    assertEquals("Duplicate a sentence.Duplicate a sentence.", manipulator.duplicateString("Duplicate a sentence."));
+    assertEquals("DuplicateDuplicate", manipulator.duplicateString("Duplicate"));
+    assertEquals("a", manipulator.duplicateString("a"));
+    assertEquals("A", manipulator.duplicateString("A"));
+    assertEquals("", manipulator.duplicateString(""));
+  }
 
-  // Test both ways to exhaust test, since the method is used for both encryption and decryption.
+  // Testing both ways to exhaust test, since the method is used for both encryption and decryption.
   @Test
-  public void testSwapCase() {}
+  public void testSwapCase() {
+    assertEquals("iN A SENTENCE.", manipulator.swapCase("In a sentence."));
+    assertEquals("In a sentence.", manipulator.swapCase("iN A SENTENCE."));
+    assertEquals("sWAPcASE", manipulator.swapCase("SwapCase"));
+    assertEquals("SwapCase", manipulator.swapCase("sWAPcASE"));
+    assertEquals("A", manipulator.swapCase("a"));
+    assertEquals("a", manipulator.swapCase("A"));
+    assertEquals("!", manipulator.swapCase("!"));
+    assertEquals("", manipulator.swapCase(""));
+  }
 
   @Test
   public void testRotateCharacters() {}
