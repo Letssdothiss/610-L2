4d00fb7af4cd1306e0ac0bc603dbb18dd371ddb1
Emil Jonasson Östvall <ej224jt@student.lnu.se>
2024-09-30 02:12:30 +0200

    JUnit setup, filestructure for testing, prepare some comments on what to implement.

---
+++ src/Test/java/stringCryption/StringCryptionTest.java
@@ -0,0 +1,5 @@
+package Test.java.stringCryption;
+
+public class StringCryptionTest {
+    
+}
---
+++ src/Test/java/stringCryption/StringManipulator/StringManipulatorTest.java
@@ -0,0 +1,34 @@
+package Test.java.stringCryption.StringManipulator;
+
+import main.java.stringCryption.StringManipulator.StringManipulator;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class StringManipulatorTest {
+    
+  // testReverseString - both ways to exhaust test, since the methos is used for both encryption and decryption
+
+  // testDuplicateString
+
+  // testSwapCase
+
+  // testRotateCharacters
+
+  // testVowelsToSymbols
+
+  // testInsertPhrase
+
+  // testStringToCamelCase
+
+  // testUndoDuplicateString
+
+  // testUndoRotateCharacters
+
+  // testUndoVowelsToSymbolsg
+
+  // testUndoInsertPhrase
+
+  // testUndoStringToCamelCase
+
+}
