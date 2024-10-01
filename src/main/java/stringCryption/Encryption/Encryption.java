fb893d5e27e5c3f7431a5be1594ebfb32c5bf74d
Emil Jonasson Östvall <ej224jt@student.lnu.se>
2024-10-01 20:28:22 +0200

    Initial method definitions in place, more to come as the ciphers are broken down.

--- src/main/java/stringCryption/Encryption/Encryption.java
+++ src/main/java/stringCryption/Encryption/Encryption.java
@@ -2,17 +2,28 @@ package main.java.stringCryption.Encryption;
 
 public class Encryption {
   // Substitution List for Substitution Cipher
+  private final String SUBSTITUTION_LIST = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;':,.<>?";
+
   // Keyword for Vigenere Cipher
+  private final String VIGENERE_KEYWORD = "ehCHrDGqvhtrBmF";
 
   /**
-   * Ceaser Cipher Encryption
+   * Caesar Cipher Encryption
    */
+  public String caesarCipherEncryption(String input, int timesToShift) {}
 
+  public String caesarCipherDecryption(String input, int timesToShift) {}
   /**
    * Substitution Cipher Encryption
    */
+  public String substitutionCipherEncryption(String input) {}
+
+  public String substitutionCipherDecryption(String input) {}
 
   /**
    * Vigenere Cipher Encryption
    */
+  public String vigenereCipherEncryption(String input, String keyword) {}
+
+  public String vigenereCipherDecryption(String input, String keyword) {}
 }
