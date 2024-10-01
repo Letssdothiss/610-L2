c64f8ef93ac717771f67d90eb916dc2eb0ad608a
Emil Jonasson Östvall <ej224jt@student.lnu.se>
2024-10-02 00:35:22 +0200

    Encryption levels implemented, decryption next.

--- src/main/java/stringCryption/Encryption/Encryption.java
+++ src/main/java/stringCryption/Encryption/Encryption.java
@@ -17,17 +17,26 @@ public class Encryption {
 
   // Level 1 Encryption.
   public String levelOneEncryption(String input) {
-    return input;
+    int timesToShift = 9;
+    int saltLength = 32;
+    String encryptedInput = stringManipulator.reverseString(input);
+    encryptedInput = encryptionCipher.caesarCipherEncryption(encryptedInput, timesToShift);
+    encryptedInput = salt.addSalt(encryptedInput, saltLength);
+    return encryptedInput;
   }
 
   // Level 1 Decryption.
+  int timesToShift = 9;
+  int saltLength = 32;
   public String levelOneDecryption(String encryptedInput) {
     return encryptedInput;
   }
 
   // Level 2 Encryption.
   public String levelTwoEncryption(String input) {
-    return input;
+    String encryptedInput = levelOneEncryption(input);
+    encryptedInput = stringManipulator.duplicateString(encryptedInput);
+    return encryptedInput;
   }
 
   // Level 2 Decryption.
@@ -37,7 +46,9 @@ public class Encryption {
 
   // Level 3 Encryption.
   public String levelThreeEncryption(String input) {
-    return input;
+    String encryptedInput = levelTwoEncryption(input);
+    encryptedInput = stringManipulator.undoVowelsToSymbols(encryptedInput);
+    return encryptedInput;
   }
 
   // Level 3 Decryption.
@@ -47,6 +58,9 @@ public class Encryption {
 
   // Level 4 Encryption.
   public String levelFourEncryption(String input) {
+    int timesToShift = 6;
+    String encryptedInput = levelThreeEncryption(input);
+    encryptedInput = encryptionCipher.caesarCipherEncryption(encryptedInput, timesToShift);
     return input;
   }
 
@@ -57,7 +71,11 @@ public class Encryption {
   
   // Level 5 Encryption.
   public String levelFiveEncryption(String input) {
-    return input;
+    int saltLength = 16;
+    String encryptedInput = levelFourEncryption(input);
+    encryptedInput = salt.addSalt(encryptedInput, saltLength);
+    encryptedInput = stringManipulator.swapCase(encryptedInput);
+    return encryptedInput;
   }
 
   // Level 5 Decryption.
