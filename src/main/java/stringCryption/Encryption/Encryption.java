4d0e5e473db0a03f12ce116c331df5b22f502bbf
Emil Jonasson Östvall <ej224jt@student.lnu.se>
2024-10-01 21:06:10 +0200

    First encryption cipher implemented, creating test next.

--- src/main/java/stringCryption/Encryption/Encryption.java
+++ src/main/java/stringCryption/Encryption/Encryption.java
@@ -9,19 +9,79 @@ public class Encryption {
 
   /**
    * Caesar Cipher Encryption
+   * 
+   * A simpler encryption technique that shifts the characters by a specified number of times.
+   * 
+   * If this cipher is used to create a custom encryption, 
+   * the shift value should be remembered and kept secret.
+   *
+   * @param input The string to encrypt.
+   * @param timesToShift The number of times to shift the characters.
+   * @return The encrypted string.
+   * @throws IllegalArgumentException If the input string is empty or contains only spaces.
+   * @throws IllegalArgumentException If the shift value is less than 1.
    */
-  public String caesarCipherEncryption(String input, int timesToShift) {}
+  public String caesarCipherEncryption(String input, int timesToShift) {
+    if (input.length() == 0 || input == " ") {
+      throw new IllegalArgumentException("Input string must contain atleast one character.");
+    } else if (timesToShift < 1) {
+      throw new IllegalArgumentException("Shift value must be atleast 1.");
+    }
+    StringBuilder encryptedInput = new StringBuilder();
+    for (char characterInInput : input.toCharArray()) {
+      encryptedInput.append(isCharacterLetter(characterInInput, timesToShift));
+    }
+    return encryptedInput.toString();
+  }
 
-  public String caesarCipherDecryption(String input, int timesToShift) {}
+  private char isCharacterLetter(char characterInInput, int timesToShift) {
+    if (Character.isLetter(characterInInput)) {
+      return shiftLetter(characterInInput, timesToShift);
+    } else {
+      return characterInInput;
+    }
+  }
+
+  private char shiftLetter(char letterToShift, int timesToShift) {
+    int alphabetLength = 26;
+    // Determine the base character based on the case of the letter.
+    char baseCharacter = Character.isLowerCase(letterToShift) ? 'a' : 'A';
+    // Shifting the letter in the alphabet, using modulo to make sure 
+    // the letter cant go beyond the alphabet.
+    return (char) (((letterToShift - baseCharacter + timesToShift) % alphabetLength) + baseCharacter);
+  }
+
   /**
-   * Substitution Cipher Encryption
+   * Caesar Cipher Decryption
+   * 
+   * A simpler decryption technique that shifts the characters back by a specified number of times.
+   * 
+   * If this cipher is used to create a custom encryption,
+   * the same shift value used for encryption must be used for decryption. 
+   *
+   * @param encryptedInput The string to decrypt.
+   * @param timesToShift The number of times to shift the characters back.
+   * @return The decrypted string.
+   * @throws IllegalArgumentException If the input string is empty or contains only spaces.
+   * @throws IllegalArgumentException If the shift value is less than 1.
+   */
+  public String caesarCipherDecryption(String encryptedInput, int timesToShift) {
+    if (encryptedInput.length() == 0 || encryptedInput == " ") {
+      throw new IllegalArgumentException("Input string must contain atleast one character.");
+    } else if (timesToShift < 1) {
+      throw new IllegalArgumentException("Shift value must be atleast 1.");
+    }
+    return caesarCipherEncryption(encryptedInput, 26 - timesToShift);
+  }
+  /**
+   * Substitution Cipher Encryption & Decryption
    */
   public String substitutionCipherEncryption(String input) {}
 
   public String substitutionCipherDecryption(String input) {}
 
   /**
-   * Vigenere Cipher Encryption
+   * Vigenere Cipher Encryption & Decryption
    */
   public String vigenereCipherEncryption(String input, String keyword) {}
 
