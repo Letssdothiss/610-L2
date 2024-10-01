c5e0ad180b224f128ac96e8859b6df3c0b8dc585
Emil Jonasson Östvall <ej224jt@student.lnu.se>
2024-10-02 00:01:14 +0200

    Structure for encryption leves defined.

--- src/Test/java/stringCryption/Encryption/EncryptionTest.java
+++ src/Test/java/stringCryption/Encryption/EncryptionTest.java
@@ -1,5 +1,11 @@
 package Test.java.stringCryption.Encryption;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.java.stringCryption.Encryption.Encryption;
+
 public class EncryptionTest {
     
 }
--- src/main/java/stringCryption/Encryption/Encryption.java
+++ src/main/java/stringCryption/Encryption/Encryption.java
@@ -1,5 +1,67 @@
 package main.java.stringCryption.Encryption;
 
+import main.java.stringCryption.EncryptionCipher.EncryptionCipher;
+import main.java.stringCryption.Salt.Salt;
+import main.java.stringCryption.StringManipulator.StringManipulator;
+
 public class Encryption {
-    
+  private EncryptionCipher encryptionCipher;
+  private Salt salt;
+  private StringManipulator stringManipulator;
+
+  public Encryption() {
+    this.encryptionCipher = new EncryptionCipher();
+    this.salt = new Salt();
+    this.stringManipulator = new StringManipulator();
+  }
+
+  // Level 1 Encryption.
+  public String levelOneEncryption(String input) {
+    return input;
+  }
+
+  // Level 1 Decryption.
+  public String levelOneDecryption(String encryptedInput) {
+    return encryptedInput;
+  }
+
+  // Level 2 Encryption.
+  public String levelTwoEncryption(String input) {
+    return input;
+  }
+
+  // Level 2 Decryption.
+  public String levelTwoDecryption(String encryptedInput) {
+    return encryptedInput;
+  }
+
+  // Level 3 Encryption.
+  public String levelThreeEncryption(String input) {
+    return input;
+  }
+
+  // Level 3 Decryption.
+  public String levelThreeDecryption(String encryptedInput) {
+    return encryptedInput;
+  }
+
+  // Level 4 Encryption.
+  public String levelFourEncryption(String input) {
+    return input;
+  }
+
+  // Level 4 Decryption.
+  public String levelFourDecryption(String encryptedInput) {
+    return encryptedInput;
+  }
+  
+  // Level 5 Encryption.
+  public String levelFiveEncryption(String input) {
+    return input;
+  }
+
+  // Level 5 Decryption.
+  public String levelFiveDecryption(String encryptedInput) {
+    return encryptedInput;
+  }
 }
