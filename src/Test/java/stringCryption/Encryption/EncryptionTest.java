b7e89b264f92a98c5fc0628d992b45158bb3e706
Emil Jonasson Östvall <ej224jt@student.lnu.se>
2024-10-01 20:05:55 +0200

    Preparations for adding simple encryption models.

---
+++ src/Test/java/stringCryption/Encryption/EncryptionTest.java
@@ -0,0 +1,5 @@
+package Test.java.stringCryption.Encryption;
+
+public class EncryptionTest {
+    
+}
--- src/main/java/stringCryption/Encryption/Encryption.java
+++ src/main/java/stringCryption/Encryption/Encryption.java
@@ -1,5 +1,18 @@
 package main.java.stringCryption.Encryption;
 
 public class Encryption {
-    
+  // Substitution List for Substitution Cipher
+  // Keyword for Vigenere Cipher
+
+  /**
+   * Ceaser Cipher Encryption
+   */
+
+  /**
+   * Substitution Cipher Encryption
+   */
+
+  /**
+   * Vigenere Cipher Encryption
+   */
 }
