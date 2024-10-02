657e41b3b21d3909611fe170e8c9ebb409f272fb
Emil Jonasson Östvall <ej224jt@student.lnu.se>
2024-10-02 10:55:21 +0200

    Encryption levels tested and passed.

--- src/Test/java/stringCryption/StringCryptionTest.java
+++ src/Test/java/stringCryption/StringCryptionTest.java
@@ -8,6 +8,7 @@ import main.java.stringCryption.StringCryption;
 
 public class StringCryptionTest {
   private StringCryption stringCryption;
+  private String testInput = "This is a test string. It should be encrypted and decrypted. Some symbols and white spaces: !@#$%^&*()_+{}|:\"<>?[];',./\\      ";
 
     @BeforeEach
     public void setUp() {
@@ -15,17 +16,37 @@ public class StringCryptionTest {
     }
 
     @Test
-    public void testEncryptDecryptLevel1() {}
+    public void testEncryptDecryptLevel1() {
+      String encrypted = stringCryption.encryptAtChosenLevel(testInput, 1);
+      String decrypted = stringCryption.decryptAtChosenLevel(encrypted, 1);
+      assertEquals(testInput, decrypted);
+    }
 
     @Test
-    public void testEncryptDecryptLevel2() {}
+    public void testEncryptDecryptLevel2() {
+      String encrypted = stringCryption.encryptAtChosenLevel(testInput, 2);
+      String decrypted = stringCryption.decryptAtChosenLevel(encrypted, 2);
+      assertEquals(testInput, decrypted);
+    }
 
     @Test
-    public void testEncryptDecryptLevel3() {}
+    public void testEncryptDecryptLevel3() {
+      String encrypted = stringCryption.encryptAtChosenLevel(testInput, 3);
+      String decrypted = stringCryption.decryptAtChosenLevel(encrypted, 3);
+      assertEquals(testInput, decrypted);
+    }
 
     @Test
-    public void testEncryptDecryptLevel4() {}
+    public void testEncryptDecryptLevel4() {
+      String encrypted = stringCryption.encryptAtChosenLevel(testInput, 4);
+      String decrypted = stringCryption.decryptAtChosenLevel(encrypted, 4);
+      assertEquals(testInput, decrypted);
+    }
 
     @Test
-    public void testEncryptDecryptLevel5() {}
+    public void testEncryptDecryptLevel5() {
+      String encrypted = stringCryption.encryptAtChosenLevel(testInput, 5);
+      String decrypted = stringCryption.decryptAtChosenLevel(encrypted, 5);
+      assertEquals(testInput, decrypted);
+    }
 }
